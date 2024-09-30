776df67d3fa89b458ab8a9ece08c99a0b178c43e
AndersonCamposs <anderson.camposs87@outlook.com>
2024-09-30 20:11:03 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,66 @@
+<!--
+CRIAR O TEMPLATE DO GOOGLE
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Google</title>
+        <link rel="stylesheet" href="./css/styles.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <a href="#">Gmail</a>
+            <a href="#">Imagens</a>
+            <i class="bi bi-person-circle"></i>
+        </div>
+        <div class="main">
+            <img src="./img/google.png"/>
+            <div class="container">
+                <div class="search-input">
+                    <i class="bi bi-search"></i>
+                    <input type="text"/> 
+                    <i class="bi bi-keyboard"></i>
+                    <i class="bi bi-mic"></i>
+                    <i class="bi bi-camera"></i>
+                </div>
+                <div class="buttons">
+                    <input type="button" value="Pesquisa no Google"/>
+                    <input type="button" value="Estou com sorte"/>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div>
+                <p>Brasil</p>
+            </div>
+            <div style="border-top: 1px solid rgb(215, 215, 215)">
+                <div>
+                    <a href="https://about.google/?utm_source=google-BR&utm_medium=referral&utm_campaign=hp-footer&fg=1">
+                        Sobre
+                    </a>
+                    <a href="https://ads.google.com/intl/pt-BR_br/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1">
+                        Publicidade
+                    </a>
+                    <a href="https://smallbusiness.withgoogle.com/intl/pt-BR_br/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google&utm_medium=ep&utm_campaign=google_hpbfooter&utm_content=google_hpbfooter&gmbsrc=ww-ww-et-gs-z-gmb-s-z-u~sb-g4sb_srvcs-u&c=BR#!/">
+                        Negócios
+                    </a>
+                    <a href="https://www.google.com/search/howsearchworks/?fg=1">
+                        Como funciona a pesquisa
+                    </a>
+                </div>
+                <div>
+                    <a href="https://policies.google.com/privacy?hl=pt-BR&fg=1">
+                        Privacidade
+                    </a>
+                    <a href="https://policies.google.com/terms?hl=pt-BR&fg=1">
+                        Termos
+                    </a>
+                    <a href="#">
+                        Configurações
+                    </a>                   
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
